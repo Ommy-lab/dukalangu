ea7efe7ece4538b067a7459365ebdbc70b1ce4ae
Ommy-lab <omaryhassan2004@gmail.com>
2026-07-03 00:33:41 +0300

    Modified db

--- includes/db_connect.php
+++ includes/db_connect.php
@@ -1,11 +1,12 @@
 <?php
 
-$host = "localhost";
-$username = "root";
-$password = "";
-$database = "duka_bora";
+$host = "db.fr-pari1.bengt.wasmernet.com";
+$username = "user_bcc36531";
+$password = "pw_BeOuLvzbhrYjHIgGfQUmHqtDIi8IZnHu";
+$port = "10272";
+$database = "db_7e0820ef";
 
-$conn = mysqli_connect($host, $username, $password, $database);
+$conn = mysqli_connect($host, $username, $password, $database, $port);
 
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
